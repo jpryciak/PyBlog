6c2e021285e4e91a38e3b99b21334febaa10912d
Your Name <jpryciak@gmail.com>
2025-02-16 15:37:48 -0500

    Added fake data, styling, django templates.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Jeff P',
+        'title': 'Python',
+        'content': 'Beautiful is better than ugly',
+        'date_posted' : 'Feb 16, 2025'
+    },
+{
+        'author': 'Jeff P',
+        'title': 'C#',
+        'content': 'ugly is better than beautiful',
+        'date_posted' : 'Feb 10, 2025'
+    }
+]
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title' : 'About'})
